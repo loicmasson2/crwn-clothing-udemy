80e186d1b6b94751ba35169f17fefee8f03b7ce3
Loic Masson <loic.masson@blueprintgenetics.com>
2019-10-10 11:11:02 +0300

    Forgot to push new files

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,27 @@
+import CartActionsType from './cart.types';
+
+import { addItemToCart } from './cart.utils';
+ 
+const INITIAL_STATE = {
+    hidden: true,
+    cartItems: []
+}
+
+const cartReducer = ( state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case CartActionsType.TOGGLE_CART_HIDDEN:
+            return {
+                ...state,
+                hidden: !state.hidden
+            }
+        case CartActionsType.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        default:
+            return state;
+    }
+}
+
+export default cartReducer;
